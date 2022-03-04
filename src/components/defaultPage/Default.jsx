edb228b7436dc06f63c9876aedc802ca0d5500fc
Vitalii <vitalik.blablabla00@gmail.com>
2022-03-04 12:37:06 +0200

    cards added

--- src/components/defaultPage/Default.jsx
+++ src/components/defaultPage/Default.jsx
@@ -1,9 +1,10 @@
-import { Button, Col, Row } from "react-bootstrap"
+import { Button, Card, Container, Col, Row } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 import { getUserData, logout } from "../../storeAsyncActions/account"
 import { useSelector, useDispatch } from "react-redux"
 import { useEffect, useState } from "react"
 import { getDataAction } from './../../store/authReducer'
+import { getTrending } from "../../storeAsyncActions/movies"
 
 
 const DefaultPage = () => {
@@ -16,10 +17,28 @@ const DefaultPage = () => {
 
 	const [movies, setMovies] = useState([])
 
-	//useEffect()
+	useEffect(() => {
+		getTrending().then(res => {
+			const arr = res.results.map((el) => {
+				return (
+					<Card className={'overflow-hidden bg-dark card border-secondary mb-1 text-white'} style={{ width: '15rem', height: '18rem' }} onClick={() => { console.log('click') }}>
+						<Card.Img variant="top" src={`https://image.tmdb.org/t/p/original${el.backdrop_path}`} />
+						<Card.Body >
+							<Card.Title>{el.original_title}</Card.Title>
+							<Card.Text >
+								<div >
+									{el.overview}
+								</div>
+							</Card.Text>
+						</Card.Body>
+					</Card >)
+			})
+			debugger
+			setMovies(arr)
+		})
+	}, [userData?.id])
 
 	useEffect(() => {
-		debugger
 		if (localStorage.getItem('userToken')) {
 			getUserData(localStorage.getItem('userToken')).then(res => {
 				const payload = {
@@ -31,9 +50,11 @@ const DefaultPage = () => {
 	}, [userData?.id])
 
 	return (
-		<div>
-			<h3>default page</h3>
-		</div>
+		<Container className='d-flex p-2' fluid={true} xl={10} lg={10} md={10} sm={10} xs={10} >
+			<Row className='d-flex justify-content-center'>
+				{movies}
+			</Row>
+		</Container>
 	)
 }
 
--- src/storeAsyncActions/movies.js
+++ src/storeAsyncActions/movies.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { variables } from "../variables"
 
-export const getTrending = () => {
-    let response = axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${variables.API_KEY}`)
-    console.log(response)
+export const getTrending = async () => {
+    let response = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${variables.API_KEY}`)
+    return response.data
 }
