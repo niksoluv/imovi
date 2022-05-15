6746fda139839e897c8f4b2ccc427d3128eee1e7
niksoluv <vitalik.blablabla00@gmail.com>
2022-05-15 03:04:14 +0300

    commetns reply update

--- src/components/details/comments/CommentsSection.jsx
+++ src/components/details/comments/CommentsSection.jsx
@@ -91,7 +91,7 @@ const CommentsSection = (props) => {
   }
 
   const mapComments = (comments, userId, movieId) => {
-    const mappedComments = comments.map(comment => {
+    const mappedComments = comments?.map(comment => {
       const usersLikes = comment.usersLikes
       const commentLiked = usersLikes.filter(ul => ul.userId === userId).length > 0 ? true : false
 
@@ -99,7 +99,7 @@ const CommentsSection = (props) => {
       const date = new Date(comment.date)
       return (
         <>
-          <div className="d-flex flex-start">
+          <div className="d-flex flex-start ">
             <img className="rounded-circle shadow-1-strong me-3"
               src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(23).webp" alt="avatar" width="60"
               height="60" />
@@ -139,14 +139,179 @@ const CommentsSection = (props) => {
                               dispatch(getCommentsAction(payload))
                             })
                           })
-                    }}></i>
+                    }}>{` ${comment.usersLikes.length}`}</i>
                 </div>
               </div>
-              <p className="mb-0">
-                {comment.data}
-              </p>
             </div>
           </div>
+          <div className="mb-0 text-wrap">
+            {comment.data}
+          </div>
+
+          <Row>
+            <Col xs={1} xl={1} lg={1} md={1} sm={1}></Col>
+            <Col xs={10} xl={10} lg={10} md={10} sm={10}>
+            <div className="d-flex flex-start ">
+            <img className="rounded-circle shadow-1-strong me-3"
+              src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(23).webp" alt="avatar" width="60"
+              height="60" />
+            <div>
+              <h6 className="fw-bold mb-1">{comment.user.username}</h6>
+              <div className="d-flex align-items-center mb-3">
+                <p className="mb-0 m-1">
+                  {date.toDateString()}
+                </p>
+                {comment.user.id === userId ?
+                  <div className={`btn-sm link-muted ${styles.button}`}><i className="fa fa-pencil-alt "></i></div>
+                  :
+                  <></>
+                }
+                <div className={`btn-sm link-muted ${styles.button}`}><i className="fas fa-redo-alt ms-2"></i></div>
+                <span>
+                <div className={`btn-sm link-muted ${commentLiked ? styles.button_red : styles.button}`}>
+                  <i className={`fas fa-heart ms-2 no-wrap`}
+                    onClick={() => {
+                      if (userInfo?.id === undefined) {
+                        notify_warn("You need to be logged in to like or unlike comments")
+                        return
+                      }
+                      commentLiked ? unlikeComment(comment.id, movieId)
+                        .then(res => {
+                          getComments(props.state.id).then(res => {
+                            const payload = {
+                              comments: res.response
+                            }
+                            dispatch(getCommentsAction(payload))
+                          })
+                        }) : likeComment(comment.id, movieId)
+                          .then(res => {
+                            getComments(props.state.id).then(res => {
+                              const payload = {
+                                comments: res.response
+                              }
+                              dispatch(getCommentsAction(payload))
+                            })
+                          })
+                    }}>{` ${comment.usersLikes.length}`}</i>
+                </div></span>
+              </div>
+            </div>
+          </div>
+          <div className="mb-0 text-wrap">
+            {comment.data}
+          </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={1} xl={1} lg={1} md={1} sm={1}></Col>
+            <Col xs={10} xl={10} lg={10} md={10} sm={10}>
+            <div className="d-flex flex-start ">
+            <img className="rounded-circle shadow-1-strong me-3"
+              src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(23).webp" alt="avatar" width="60"
+              height="60" />
+            <div>
+              <h6 className="fw-bold mb-1">{comment.user.username}</h6>
+              <div className="d-flex align-items-center mb-3">
+                <p className="mb-0 m-1">
+                  {date.toDateString()}
+                </p>
+                {comment.user.id === userId ?
+                  <div className={`btn-sm link-muted ${styles.button}`}><i className="fa fa-pencil-alt "></i></div>
+                  :
+                  <></>
+                }
+                <div className={`btn-sm link-muted ${styles.button}`}><i className="fas fa-redo-alt ms-2"></i></div>
+                <span>
+                <div className={`btn-sm link-muted ${commentLiked ? styles.button_red : styles.button}`}>
+                  <i className={`fas fa-heart ms-2 no-wrap`}
+                    onClick={() => {
+                      if (userInfo?.id === undefined) {
+                        notify_warn("You need to be logged in to like or unlike comments")
+                        return
+                      }
+                      commentLiked ? unlikeComment(comment.id, movieId)
+                        .then(res => {
+                          getComments(props.state.id).then(res => {
+                            const payload = {
+                              comments: res.response
+                            }
+                            dispatch(getCommentsAction(payload))
+                          })
+                        }) : likeComment(comment.id, movieId)
+                          .then(res => {
+                            getComments(props.state.id).then(res => {
+                              const payload = {
+                                comments: res.response
+                              }
+                              dispatch(getCommentsAction(payload))
+                            })
+                          })
+                    }}>{` ${comment.usersLikes.length}`}</i>
+                </div></span>
+              </div>
+            </div>
+          </div>
+          <div className="mb-0 text-wrap">
+            {comment.data}
+          </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={1} xl={1} lg={1} md={1} sm={1}></Col>
+            <Col xs={10} xl={10} lg={10} md={10} sm={10}>
+            <div className="d-flex flex-start ">
+            <img className="rounded-circle shadow-1-strong me-3"
+              src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(23).webp" alt="avatar" width="60"
+              height="60" />
+            <div>
+              <h6 className="fw-bold mb-1">{comment.user.username}</h6>
+              <div className="d-flex align-items-center mb-3">
+                <p className="mb-0 m-1">
+                  {date.toDateString()}
+                </p>
+                {comment.user.id === userId ?
+                  <div className={`btn-sm link-muted ${styles.button}`}><i className="fa fa-pencil-alt "></i></div>
+                  :
+                  <></>
+                }
+                <div className={`btn-sm link-muted ${styles.button}`}><i className="fas fa-redo-alt ms-2"></i></div>
+                <span>
+                <div className={`btn-sm link-muted ${commentLiked ? styles.button_red : styles.button}`}>
+                  <i className={`fas fa-heart ms-2 no-wrap`}
+                    onClick={() => {
+                      if (userInfo?.id === undefined) {
+                        notify_warn("You need to be logged in to like or unlike comments")
+                        return
+                      }
+                      commentLiked ? unlikeComment(comment.id, movieId)
+                        .then(res => {
+                          getComments(props.state.id).then(res => {
+                            const payload = {
+                              comments: res.response
+                            }
+                            dispatch(getCommentsAction(payload))
+                          })
+                        }) : likeComment(comment.id, movieId)
+                          .then(res => {
+                            getComments(props.state.id).then(res => {
+                              const payload = {
+                                comments: res.response
+                              }
+                              dispatch(getCommentsAction(payload))
+                            })
+                          })
+                    }}>{` ${comment.usersLikes.length}`}</i>
+                </div></span>
+              </div>
+            </div>
+          </div>
+          <div className="mb-0 text-wrap">
+            {comment.data}
+          </div>
+            </Col>
+          </Row>
+
+
 
           <hr className="mt-1" />
         </>
@@ -197,19 +362,15 @@ const CommentsSection = (props) => {
             </Col>
           </Row>
         </Container>
-        <Container className="p-s">
-          <Row className="d-flex justify-content-center">
-            <Col className="md-12 col-lg-10">
-              <Card className="text-dark">
-                <Card.Body className="p-4">
-                  <h4 className="fw-light pb-2">Recent comments</h4>
+        <Container>
+          <Card className="text-dark">
+            <Card.Body className="p-4">
+              <h4 className="fw-light pb-2">Recent comments</h4>
 
-                  {comments}
+              {comments}
 
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+            </Card.Body>
+          </Card>
 
         </Container>
       </section>
