4e48857482c3bc6929a5faaadcce7de56b84649a
niksoluv <vitalik.blablabla00@gmail.com>
2022-06-22 12:02:39 +0300

    tests added

--- src/tests/mediaType.test.js
+++ src/tests/mediaType.test.js
@@ -8,3 +8,27 @@ test("defines media type of movie", () => {
   }
   expect(defineMediatype(media)).toBe("movie")
 })
+
+test("defines media type of movie", () => {
+  const media = {
+    number_of_seasons: undefined,
+    first_air_date: '22-02-2222'
+  }
+  expect(defineMediatype(media)).toBe("tv")
+})
+
+test("defines media type of movie", () => {
+  const media = {
+    number_of_seasons: 1,
+    first_air_date: undefined
+  }
+  expect(defineMediatype(media)).toBe("tv")
+})
+
+test("defines media type of tv", () => {
+  const media = {
+    number_of_seasons: 1,
+    first_air_date: '22-02-2222'
+  }
+  expect(defineMediatype(media)).toBe("tv")
+})
