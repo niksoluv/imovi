0a13b981b00d4a21e1195743380ddd5ff2065acf
zanudas <vitalik.blablabla00@gmail.com>
2023-08-11 16:46:29 +0300

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,24 +1,24 @@
-import '../../App.css';
+import "../../App.css";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="bg-dark footer--pin text-white text-center">© 2022 Copyright:
+      <div className="bg-dark footer--pin text-white text-center">
+        © 2022 Copyright:
         <br />
         <div class="h-card">
-          <a class="p-name u-url"
-            href="https://vitalii.hladkyi/"
-          >Гладкий Віталій</a>
-          (<a class="p-org h-card"
-            href="https://sigma.software/"
-          >Sigma software</a>)
-        </div><p class="h-geo">
-          <span class="p-latitude">-27.116667</span>,
-          <span class="p-longitude">-109.366667</span>
-        </p>
+          <a class="p-name u-url" href="https://vitalii.hladkyi/">
+            Гладкий Віталій
+          </a>
+          (
+          <a class="p-org h-card" href="https://sigma.software/">
+            Sigma software
+          </a>
+          )
+        </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
