f1b4b421dc30d628ef8fab6f36f7dcf25a5b834d
zanudas <vitalik.blablabla00@gmail.com>
2023-08-11 16:40:10 +0300

    build script added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,17 @@ const Footer = () => {
     <footer>
       <div className="bg-dark footer--pin text-white text-center">© 2022 Copyright:
         <br />
-        <span style={{ fontSize:"x-large" }} className="bg-dark text-white"> imovi </span>
+        <div class="h-card">
+          <a class="p-name u-url"
+            href="https://vitalii.hladkyi/"
+          >Гладкий Віталій</a>
+          (<a class="p-org h-card"
+            href="https://sigma.software/"
+          >Sigma software</a>)
+        </div><p class="h-geo">
+          <span class="p-latitude">-27.116667</span>,
+          <span class="p-longitude">-109.366667</span>
+        </p>
       </div>
     </footer>
   )
